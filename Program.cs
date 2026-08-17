db0bc480b363d328f3be746157d255ae72143247
ahmed23jd4-star <ahmed23jd4@gmail.com>
2026-08-17 15:43:38 +0300

    Theoretical Questions q1

--- Program.cs
+++ Program.cs
@@ -4,14 +4,23 @@
     {
         static void Main(string[] args)
         {
-            DeliveryAddres address01 = new DeliveryAddres(5, "10 bab alloq ", "cairo");
-            DeliveryAddres address02 = address01;
-            address01.GetFullAddress();
-            address02.GetFullAddress();
-            address01.city = "alex";
-            address01.buildingnumber = 50;
-            address01.GetFullAddress();
-            address02.GetFullAddress();
+            #region Theoretical Questions q1
+            //What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+            //doesn,t change because struct is value type not reference 
+            //b) What happens when a Customer variable is copied into another variable and one variable modifies the object?
+            // the first the same data but the object 2 change the object 1 doesn,t change 
+
+
+
+            //DeliveryAddres address01 = new DeliveryAddres(5, "10 bab alloq ", "cairo");
+            //DeliveryAddres address02 = address01;
+            //address01.GetFullAddress();
+            //address02.GetFullAddress();
+            //address01.city = "alex";
+            //address01.buildingnumber = 50;
+            //address01.GetFullAddress();
+            //address02.GetFullAddress();
+            #endregion
         }
     }
 }
